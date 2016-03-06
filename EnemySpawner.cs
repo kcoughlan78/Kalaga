4bb7c74c936e35c553ebe519bccd521ddc5832b4
kcoughlan78 <kieran-coughlan@outlook.ie>
2016-03-06 20:30:49 +0000

    fixed mouse player move, responsively clamped player sprite to screen, added initial enemy spawner

---
+++ EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+        GameObject enemy = Instantiate(enemyPrefab, new Vector3(5.5f, 9f, -1f), Quaternion.identity) as GameObject;
+        enemy.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private bool hasStarted = false;
+    float xmin;
+    float xmax;
+
+    // Use this for initialization
+    void Start () {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftLimit = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightLimit = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftLimit.x;
+        xmax = rightLimit.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ShipControl();
+        ShipAnimate();
+	}
+
+    void ShipAnimate()
+    {
+        if(Input.GetAxis("Mouse X") < 0)
+        {
+            print("Mouse left");
+        }else if (Input.GetAxis("Mouse X") > 0)
+        {
+            print("Mouse right");
+        }else if (Input.GetAxis("Mouse X") == 0)
+        {
+            print("Mouse Still");
+        }   
+    }
+
+    void ShipControl()
+    {
+        Vector3 ShipPos = new Vector3(0.29f, this.transform.position.y, 0f);
+        float MousePosBlocks = (Input.mousePosition.x / Screen.width) * 11;
+        ShipPos.x = Mathf.Clamp(MousePosBlocks, xmin, xmax);
+        this.transform.position = ShipPos;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+        }
+    }
+}
